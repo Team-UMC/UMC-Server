0b583387b7deb009c74fe61dae2f3dfdbd24ea16
KyungsooLee <jmtkd9196@gmail.com>
2023-12-28 18:33:41 +0900

    feat: backend project init

---
+++ src/main/java/com/umc/networkingService/NetworkingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.umc.networkingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingServiceApplication.class, args);
+	}
+
+}
