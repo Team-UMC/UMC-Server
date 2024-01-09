09f96cc5a104496884d92e8b2f6eea1aa9afdfdc
tnals2384 <tnals2384@gmail.com>
2024-01-09 16:15:43 +0900

    feat: BaseEntity, BaseResponse 구현

---
+++ src/main/java/com/umc/networkingService/global/common/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.umc.networkingService.global.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    // 삭제 여부 확인 메서드
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+}
