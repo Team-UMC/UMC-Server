ed27939d68d2f5f1035bf60b39462d6c67cfc746
KyungsooLee <jmtkd9196@gmail.com>
2024-01-10 02:49:30 +0900

    feat: University 엔티티 생성

---
+++ src/main/java/com/umc/networkingService/domain/university/entity/University.java
@@ -0,0 +1,36 @@
+package com.umc.networkingService.domain.university.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@RequiredArgsConstructor
+public class University {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+//    Mascot 테이블과 연결
+//    @Column(nullable = false)
+//    private UUID mascot;
+
+    private String universityLogo;
+
+    private String semesterLogo;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Long totlaPoint;
+}
