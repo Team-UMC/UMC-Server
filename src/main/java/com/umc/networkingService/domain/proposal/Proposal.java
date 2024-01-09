7596e230c1d73a538063baf8525ae3cce0775e23
tnals2384 <tnals2384@gmail.com>
2024-01-09 16:50:28 +0900

    feat: Proposal 엔티티 생성

---
+++ src/main/java/com/umc/networkingService/domain/proposal/Proposal.java
@@ -0,0 +1,37 @@
+package com.umc.networkingService.domain.proposal;
+
+import com.umc.networkingService.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Proposal extends BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    //member 테이블과 연결
+    // private Member writer;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    //@Column(nullable = false) //이미지 필수 저장
+    //private List<String> images;
+}
