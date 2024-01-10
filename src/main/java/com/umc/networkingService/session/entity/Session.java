1cb239c76288cd415dd213fb0301fd9423f5e462
tnals2384 <tnals2384@gmail.com>
2024-01-10 21:25:59 +0900

    feat: Session 연관관계 매핑

--- src/main/java/com/umc/networkingService/session/entity/Session.java
+++ src/main/java/com/umc/networkingService/session/entity/Session.java
@@ -1,15 +1,13 @@
 package com.umc.networkingService.session.entity;
 
+import com.umc.networkingService.domain.member.entity.Member;
 import com.umc.networkingService.global.common.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,16 +15,15 @@ import java.util.UUID;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
 public class Session extends BaseEntity {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name="uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @UuidGenerator
+    @Column(name = "session_id")
     private UUID id;
 
-
-    //member 테이블과 연결
-    // private Member writer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
 
     @Column(nullable = false)
     private LocalDateTime startTime;
@@ -34,8 +31,7 @@ public class Session extends BaseEntity {
     @Column(nullable = false)
     private LocalDateTime endTime;
 
-    @ColumnDefault("true")
+    @Column(columnDefinition = "boolean default true")
     private boolean isConnected;
-
 
 }
