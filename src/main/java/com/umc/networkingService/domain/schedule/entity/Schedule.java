a1dd52bb25366d24797119a8dd2a3deb94288f23
tnals2384 <tnals2384@gmail.com>
2024-01-10 21:25:46 +0900

    feat: Schedule 연관관계 매핑

--- src/main/java/com/umc/networkingService/domain/schedule/entity/Schedule.java
+++ src/main/java/com/umc/networkingService/domain/schedule/entity/Schedule.java
@@ -1,17 +1,20 @@
 package com.umc.networkingService.domain.schedule.entity;
 
 import com.umc.networkingService.domain.board.entity.HostType;
+import com.umc.networkingService.domain.member.entity.Member;
 import com.umc.networkingService.global.common.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.umc.networkingService.global.common.Part;
+import com.umc.networkingService.global.common.Role;
+import com.umc.networkingService.global.common.Semester;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -19,17 +22,18 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Schedule extends BaseEntity {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name="uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @UuidGenerator
+    @Column(name = "schedule_id")
     private UUID id;
 
-
-    //member 테이블과 연결
-    // private Member writer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
 
     @Column(nullable = false)
     private String title;
+
+    private String content;
 
     @Column(nullable = false)
     private LocalDateTime startDate;
@@ -37,17 +41,35 @@ public class Schedule extends BaseEntity {
     @Column(nullable = false)
     private LocalDateTime endDate;
 
-    //role enum과 연결
-    //@Column(nullable = false)
-    //private Role rolePermission;
+    @Column(nullable = false)
+    private String timeSetting;
 
-    //part enum과 연결
-    //@Column(nullable = false)
-    //private Part partPermission;
+    @Column(nullable = false)
+    private String placeSetting;
 
-    //semester enum과 연결
-    //@Column(nullable = false)
-    //private Semester semesterPermission;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role rolePermission;
+
+    @ElementCollection(targetClass = Part.class)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(
+            name = "schedule_part_permission",
+            joinColumns = @JoinColumn(name = "schedule_id")
+    )
+    @Column(name = "part", nullable = false)
+    private List<Part> partPermission = new ArrayList<>();
+
+
+    @ElementCollection(targetClass = Semester.class)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(
+            name= "schedule_semester_permission",
+            joinColumns = @JoinColumn(name = "schedule_id")
+    )
+    @Column(name="semester",nullable = false)
+    private List<Semester> semesterPermission = new ArrayList<>();
+
 
     @Column(nullable = false)
     private HostType hostType;
