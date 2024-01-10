c327a6ea39ddd2865ceed5d09a3b5da5996021d9
tnals2384 <tnals2384@gmail.com>
2024-01-10 21:23:50 +0900

    feat: Proposal 연관관계 매핑

--- src/main/java/com/umc/networkingService/domain/proposal/entity/Proposal.java
+++ src/main/java/com/umc/networkingService/domain/proposal/entity/Proposal.java
@@ -1,15 +1,15 @@
 package com.umc.networkingService.domain.proposal.entity;
 
+import com.umc.networkingService.domain.member.entity.Member;
 import com.umc.networkingService.global.common.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -18,13 +18,13 @@ import java.util.UUID;
 public class Proposal extends BaseEntity {
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name="uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @UuidGenerator
+    @Column(name = "proposal_id")
     private UUID id;
 
-    //member 테이블과 연결
-    // private Member writer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name="member_id")
+    private Member writer;
 
     @Column(nullable = false)
     private String title;
@@ -32,6 +32,8 @@ public class Proposal extends BaseEntity {
     @Column(nullable = false)
     private String content;
 
-    //@Column(nullable = false)
-    //private List<String> images;
+    @ElementCollection
+    private List<String> images = new ArrayList<>();
+
+
 }
