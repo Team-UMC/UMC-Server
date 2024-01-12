d903138e019cf847396e76fae18aa9054c0e3244
tnals2384 <tnals2384@gmail.com>
2024-01-13 00:54:48 +0900

    style: swagger 설명 추가

--- src/main/java/com/umc/networkingService/domain/test/controller/TestController.java
+++ src/main/java/com/umc/networkingService/domain/test/controller/TestController.java
@@ -7,12 +7,14 @@ import com.umc.networkingService.global.common.base.BaseResponse;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "예제 API", description = "Swagger 테스트용 API")
+@Tag(name = "예제 API", description = "테스트용 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/test")
@@ -35,54 +37,42 @@ public class TestController {
         return "무시되는 API";
     }
 
-    private final TestService tempService;
+    private final TestService testService;
 
-    @GetMapping("/test")
-    public BaseResponse<TestResponse.TempTestDTO> testAPI() {
 
+    @Operation(summary = "성공적인 응답 반환 API", description = "테스트 문자열을 반환하는 API입니다.")
+    @ApiResponse(responseCode = "200", description = "테스트 문자열을 성공적으로 반환")
+    @GetMapping("/success")
+    public BaseResponse<TestResponse.TempTestDTO> successResponseAPI() {
         return BaseResponse.onSuccess(TestResponse.TempTestDTO.builder().testString("This is Test!").build());
-    /*
-    {
-      "timestamp": "2024-01-12T17:19:02.0172718",
-      "code": "COMMON200",
-      "message": "요청에 성공하였습니다.",
-      "result": {
-        "testString": "This is Test!"
-      }
-    }
-     */
     }
 
+
+
+    @Operation(summary = "예외처리 API", description = "flag 값에 따라 예외를 발생시키는 API입니다.")
+    @Parameter(name = "flag", description = "1인 경우 예외처리", example = "1", required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "성공"),
+            @ApiResponse(responseCode = "TEMP4001", description = "예외 처리 테스트입니다.")
+    })
     @GetMapping("/exception")
     public BaseResponse<TestResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag) {
-        tempService.CheckFlag(flag);
+        testService.CheckFlag(flag);
         return BaseResponse.onSuccess(TestResponse.TempExceptionDTO.builder()
                 .flag(flag)
                 .build());
     }
-    /*
-    {
-      "timestamp": "2024-01-12T17:27:10.1903229",
-      "code": "TEMP4001",
-      "message": "예외처리 테스트입니다."
-    }
-     */
 
-    @PostMapping("/test-validation")
+    @Operation(summary = "유효성 검사 API", description = "요청 객체의 유효성을 검사하는 API입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "성공"),
+            @ApiResponse(responseCode = "COMMON400", description = "testString가 공백일 경우")
+    })
+    @PostMapping("/validation")
     public BaseResponse<TestResponse.TempTestDTO> testValidationAPI(
             @RequestBody @Valid TestRequest.TempTestRequest request) {
         return BaseResponse.onSuccess(TestResponse.TempTestDTO.builder()
                 .testString(request.getTestString())
                 .build());
     }
-    /* testString ""로 주었을 경우
-    {
-      "timestamp": "2024-01-12T17:47:55.8893269",
-      "code": "COMMON400",
-      "message": "잘못된 요청입니다.",
-      "result": {
-        "testString": "testString은 필수입니다."
-      }
-     }
-     */
 }
