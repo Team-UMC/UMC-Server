93741f9c7cd6511f14a0e09fc4f41c2fc222638f
KyungsooLee <jmtkd9196@gmail.com>
2024-01-15 03:23:02 +0900

    feat: JwtExceptionFilter 구현 & RestApiException 필드 추가

--- src/main/java/com/umc/networkingService/global/common/exception/RestApiException.java
+++ src/main/java/com/umc/networkingService/global/common/exception/RestApiException.java
@@ -2,10 +2,13 @@ package com.umc.networkingService.global.common.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 
 @Getter
 @AllArgsConstructor
 public class RestApiException extends RuntimeException {
     private final ErrorCode errorCode;
+    private String message;
+    private final HttpStatus status;
 }
