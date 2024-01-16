5058b4bd0b4eb7c07d4183f1572d6ed0269f57ff
준써크 <junsuk8960@naver.com>
2024-01-17 02:58:04 +0900

    feat: 대학교로 현재 지부 조회 로직 구현

---
+++ src/main/java/com/umc/networkingService/domain/branch/service/BranchServiceImpl.java
@@ -0,0 +1,23 @@
+package com.umc.networkingService.domain.branch.service;
+
+import com.umc.networkingService.domain.branch.entity.Branch;
+import com.umc.networkingService.domain.branch.repository.BranchRepository;
+import com.umc.networkingService.global.common.exception.ErrorCode;
+import com.umc.networkingService.global.common.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BranchServiceImpl implements BranchService{
+
+    private final BranchRepository branchRepository;
+
+    @Override
+    public Branch loadEntity(UUID id) {
+        return branchRepository.findById(id)
+                .orElseThrow(() -> new RestApiException(ErrorCode.EMPTY_BRANCH));
+    }
+}
