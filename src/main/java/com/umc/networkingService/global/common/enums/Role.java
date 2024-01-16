ef3bbcc076564893d5ee7bf3940377290b2a3b91
준써크 <junsuk8960@naver.com>
2024-01-17 01:01:41 +0900

    fix: Role 수정

---
+++ src/main/java/com/umc/networkingService/global/common/enums/Role.java
@@ -0,0 +1,20 @@
+package com.umc.networkingService.global.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN("관리자", 0),
+    TOTAL_STAFF("총괄운영진", 1),
+    CENTER_STAFF("중앙운영진", 2),
+    BRANCH_STAFF("지부운영진", 3),
+    CAMPUS_STAFF("학교운영진", 4),
+    STAFF("운영진", 4),
+    MEMBER("챌린저", 5);
+
+    private final String toKorean;
+
+    private final int priority;
+}
