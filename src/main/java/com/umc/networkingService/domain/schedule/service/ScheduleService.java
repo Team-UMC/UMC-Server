fedbf317f460e0a7df7c2e8d6bb665fd8e6662bb
KyungsooLee <jmtkd9196@gmail.com>
2024-01-22 01:38:47 +0900

    feat: Schedule 조회 API 구현

--- src/main/java/com/umc/networkingService/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/umc/networkingService/domain/schedule/service/ScheduleService.java
@@ -1,4 +1,8 @@
 package com.umc.networkingService.domain.schedule.service;
 
+import com.umc.networkingService.domain.schedule.dto.response.ScheduleResponse.ScheduleInfo;
+import java.util.List;
+
 public interface ScheduleService {
+    public List<ScheduleInfo> getSchedulesByMonth(Long month);
 }
