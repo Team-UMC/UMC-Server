dcf8ea9839fc75697affe0f9bcec05a681496140
KyungsooLee <jmtkd9196@gmail.com>
2024-01-22 02:22:21 +0900

    fix: 캘린더 조회 API 엔드포인트 수정

--- src/main/java/com/umc/networkingService/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/umc/networkingService/domain/schedule/controller/ScheduleController.java
@@ -1,13 +1,10 @@
 package com.umc.networkingService.domain.schedule.controller;
 
-import com.umc.networkingService.domain.schedule.dto.response.ScheduleResponse;
-import com.umc.networkingService.domain.schedule.dto.response.ScheduleResponse.ScheduleInfo;
 import com.umc.networkingService.domain.schedule.dto.response.ScheduleResponse.ScheduleInfoList;
 import com.umc.networkingService.domain.schedule.service.ScheduleService;
 import com.umc.networkingService.global.common.base.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +20,10 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @Operation(summary = "캘린더 조회 API", description = "홈 화면의 달력을 조회하는 API입니다.")
-    @PostMapping
+    @PostMapping("/calendar")
     public BaseResponse<ScheduleInfoList> getSchedule(@RequestParam Long month) {
 
-        return BaseResponse.onSuccess(scheduleService.getSchedulesByMonth(month));
+        return BaseResponse.onSuccess(scheduleService.getCalendarByMonth(month));
     }
 
 }
