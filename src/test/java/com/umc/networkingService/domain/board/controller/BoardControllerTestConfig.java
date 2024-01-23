01659cb15e1e98ef2c9b237377ade806a2f1d7d9
tnals2384 <tnals2384@gmail.com>
2024-01-23 15:48:02 +0900

    refactor: testconfig 파일 생성

---
+++ src/test/java/com/umc/networkingService/domain/board/controller/BoardControllerTestConfig.java
@@ -0,0 +1,149 @@
+package com.umc.networkingService.domain.board.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.umc.networkingService.config.security.jwt.JwtTokenProvider;
+import com.umc.networkingService.domain.board.dto.response.BoardPagePostResponse;
+import com.umc.networkingService.domain.board.dto.response.BoardPagingResponse;
+import com.umc.networkingService.domain.board.entity.Board;
+import com.umc.networkingService.domain.board.entity.BoardComment;
+import com.umc.networkingService.domain.board.entity.BoardType;
+import com.umc.networkingService.domain.board.entity.HostType;
+import com.umc.networkingService.domain.board.service.BoardCommentService;
+import com.umc.networkingService.domain.board.service.BoardService;
+import com.umc.networkingService.domain.member.entity.Member;
+import com.umc.networkingService.domain.member.entity.SemesterPart;
+import com.umc.networkingService.domain.member.entity.SocialType;
+import com.umc.networkingService.domain.member.repository.MemberRepository;
+import com.umc.networkingService.domain.member.repository.SemesterPartRepository;
+import com.umc.networkingService.global.common.enums.Part;
+import com.umc.networkingService.global.common.enums.Role;
+import com.umc.networkingService.global.common.enums.Semester;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class BoardControllerTestConfig {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @MockBean
+    protected BoardService boardService;
+    @MockBean
+    protected MemberRepository memberRepository;
+    @MockBean
+    protected BoardCommentService boardCommentService;
+    @MockBean
+    protected SemesterPartRepository semesterPartRepository;
+
+
+    protected Member member;
+    protected Board board;
+    protected BoardComment comment;
+    protected String accessToken;
+    protected String refreshToken;
+
+    @BeforeEach
+    public void setUp() {
+        member = createMember();
+        board = createBoard();
+        comment = createBoardComment();
+        setToken(member);
+    }
+
+
+    private void setToken(Member member) {
+        accessToken = jwtTokenProvider.generateAccessToken(member.getId());
+        refreshToken = jwtTokenProvider.generateRefreshToken(member.getId());
+    }
+
+    protected Member createMember() {
+        return Member.builder()
+                .id(UUID.randomUUID())
+                .clientId("123456")
+                .socialType(SocialType.KAKAO)
+                .role(Role.MEMBER)
+                .name("김준석")
+                .nickname("벡스")
+                .semesterParts(createSemesterPart(member))
+                .build();
+    }
+
+    protected List<SemesterPart> createSemesterPart(Member member) {
+        List<SemesterPart> semesterParts = List.of(
+                SemesterPart.builder().member(member).part(Part.ANDROID).semester(Semester.THIRD).build(),
+                SemesterPart.builder().member(member).part(Part.SPRING).semester(Semester.FOURTH).build()
+        );
+
+        return semesterPartRepository.saveAll(semesterParts);
+    }
+
+
+    protected Board createBoard() {
+        return Board.builder()
+                .id(UUID.randomUUID())
+                .writer(member)
+                .title("제목")
+                .content("내용")
+                .semesterPermission(List.of(Semester.FIFTH))
+                .hostType(HostType.CAMPUS)
+                .boardType(BoardType.FREE)
+                .build();
+
+    }
+    protected BoardComment createBoardComment() {
+        return BoardComment.builder()
+                .id(UUID.randomUUID())
+                .writer(member)
+                .board(board)
+                .content("신나!")
+                .build();
+    }
+
+
+
+    //가상의 BoardPagingResponse 생성
+    protected BoardPagingResponse createMockBoardPagingResponse() {
+        List<BoardPagePostResponse> boardPagePostResponses = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            BoardPagePostResponse boardPagePostResponse = BoardPagePostResponse.builder()
+                    .hostType(HostType.CAMPUS)
+                    .boardType(BoardType.FREE)
+                    .title("제목")
+                    .content("내용")
+                    .writer("루시/김수민")
+                    .hitCount(1)
+                    .commentCount(1)
+                    .heartCount(1)
+                    .profileImage(".../img")
+                    .createdAt(LocalDateTime.parse("2024-01-16T14:20:15"))
+                    .build();
+            boardPagePostResponses.add(boardPagePostResponse);
+        }
+
+
+        // 가상의 페이징 정보 설정
+        return BoardPagingResponse.builder()
+                .page(1)
+                .totalPages(3)
+                .totalElements(30)
+                .boardPagePostResponses(boardPagePostResponses)
+                .isFirst(true)
+                .isLast(false)
+                .build();
+    }
+
+
+}
---
+++ src/test/java/com/umc/networkingService/domain/board/service/BoardServiceTestConfig.java
@@ -0,0 +1,187 @@
+package com.umc.networkingService.domain.board.service;
+
+import com.umc.networkingService.domain.board.entity.*;
+import com.umc.networkingService.domain.board.repository.BoardCommentRepository;
+import com.umc.networkingService.domain.board.repository.BoardFileRepository;
+import com.umc.networkingService.domain.board.repository.BoardRepository;
+import com.umc.networkingService.domain.branch.entity.Branch;
+import com.umc.networkingService.domain.branch.repository.BranchRepository;
+import com.umc.networkingService.domain.member.entity.Member;
+import com.umc.networkingService.domain.member.entity.SemesterPart;
+import com.umc.networkingService.domain.member.entity.SocialType;
+import com.umc.networkingService.domain.member.repository.MemberRepository;
+import com.umc.networkingService.domain.member.repository.SemesterPartRepository;
+import com.umc.networkingService.domain.university.entity.University;
+import com.umc.networkingService.domain.university.repository.UniversityRepository;
+import com.umc.networkingService.global.common.enums.Part;
+import com.umc.networkingService.global.common.enums.Role;
+import com.umc.networkingService.global.common.enums.Semester;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class BoardServiceTestConfig {
+    @Autowired
+    protected BoardService boardService;
+    @Autowired
+    protected BoardFileService boardFileService;
+    @Autowired
+    protected MemberRepository memberRepository;
+    @Autowired
+    protected BoardRepository boardRepository;
+    @Autowired
+    protected BoardFileRepository boardFileRepository;
+    @Autowired
+    protected BoardCommentRepository boardCommentRepository;
+    @Autowired
+    protected UniversityRepository universityRepository;
+    @Autowired
+    protected BranchRepository branchRepository;
+    @Autowired
+    protected SemesterPartRepository semesterPartRepository;
+
+
+    protected Member member;
+    protected Member campusStaff;
+    protected Board board;
+    protected University university;
+    protected Branch branch;
+    protected BoardComment comment;
+
+    @BeforeEach
+    public void setUp() {
+        university = createUniversity();
+        branch = createBranch();
+        campusStaff = createCampusStaff();
+        member = createMember();
+        board = createBoard();
+        comment = createBoardComment();
+    }
+
+    protected Member createCampusStaff() {
+        return memberRepository.save(Member.builder()
+                .id(UUID.randomUUID())
+                .clientId("123456")
+                .socialType(SocialType.KAKAO)
+                .university(university)
+                .branch(branch)
+                .role(Role.CAMPUS_STAFF)
+                .name("김준석")
+                .nickname("벡스")
+                .semesterParts(createSemesterPart(campusStaff))
+                .build());
+    }
+
+    protected Member createMember() {
+        return memberRepository.save(Member.builder()
+                .id(UUID.randomUUID())
+                .clientId("11111")
+                .socialType(SocialType.KAKAO)
+                .university(university)
+                .branch(branch)
+                .role(Role.MEMBER)
+                .name("김수민")
+                .nickname("루시")
+                .semesterParts(createSemesterPart(member))
+                .build());
+    }
+
+    protected List<SemesterPart> createSemesterPart(Member member) {
+        List<SemesterPart> semesterParts = List.of(
+                SemesterPart.builder().member(member).part(Part.SPRING).semester(Semester.FIFTH).build(),
+                SemesterPart.builder().member(member).part(Part.ANDROID).semester(Semester.THIRD).build()
+        );
+
+        return semesterPartRepository.saveAll(semesterParts);
+    }
+
+
+    protected University createUniversity() {
+        return universityRepository.save(
+                University.builder()
+                        .name("인하대학교")
+                        .build()
+        );
+    }
+
+    protected Branch createBranch() {
+        return branchRepository.save(
+                Branch.builder()
+                        .name("GACI")
+                        .description("가치 지부입니다.")
+                        .semester(Semester.FIFTH)
+                        .build()
+        );
+    }
+
+    protected Board createBoard() {
+        return boardRepository.save(Board.builder()
+                .writer(member)
+                .title("제목")
+                .content("내용")
+                .boardType(BoardType.FREE)
+                .hostType(HostType.CAMPUS)
+                .semesterPermission(List.of(Semester.FIFTH))
+                .hitCount(0)
+                .heartCount(0)
+                .commentCount(0)
+                .build());
+    }
+
+    protected void createBoardFile() {
+        List<MultipartFile> files = new ArrayList<>();
+        files.add(new MockMultipartFile("file", "filename1.jpg", "image/jpeg", "file content".getBytes()));
+        files.add(new MockMultipartFile("file", "filename2.jpg", "image/jpeg", "file content".getBytes()));
+
+        boardFileService.uploadBoardFiles(board, files);
+
+    }
+
+    protected void createBoards() {
+        for (int i = 0; i < 11; i++) {
+            boardRepository.save(Board.builder()
+                    .writer(member)
+                    .title("데모데이가 곧이네요" + i)
+                    .content("신난다~~")
+                    .boardType(BoardType.FREE)
+                    .hostType(HostType.CENTER)
+                    .semesterPermission(List.of(Semester.FIFTH))
+                    .hitCount(0)
+                    .heartCount(0)
+                    .commentCount(0)
+                    .build());
+        }
+    }
+
+    protected BoardComment createBoardComment() {
+        return boardCommentRepository.save(BoardComment.builder()
+                .writer(member)
+                .board(board)
+                .content("신나!")
+                .build());
+    }
+
+    protected void createBoardComments() {
+        for (int i = 0; i < 15; i++) {
+
+            boardCommentRepository.save(BoardComment.builder()
+                    .writer(campusStaff)
+                    .board(board)
+                    .content("좋아요." + i)
+                    .build());
+        }
+    }
+
+
+
+
+}
