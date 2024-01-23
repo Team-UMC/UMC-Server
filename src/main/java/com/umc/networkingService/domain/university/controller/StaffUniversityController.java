18870149f00059f48a92c3fc7a27630633842fff
ShimFFF <shimseawon1510@gmail.com>
2024-01-23 17:33:46 +0900

    feat: 컨트롤러 초기 세팅

---
+++ src/main/java/com/umc/networkingService/domain/university/controller/StaffUniversityController.java
@@ -0,0 +1,26 @@
+package com.umc.networkingService.domain.university.controller;
+
+import com.umc.networkingService.domain.university.service.UniversityService;
+import com.umc.networkingService.global.common.base.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+@Tag(name = "멤버 API", description = "운영진용 멤버 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/staff/universities")
+public class StaffUniversityController {
+
+    private final UniversityService universityService;
+
+    @Operation(summary = "학교 정보 조회 API",description = "학교 정보 조회 API")
+    @GetMapping("/details")
+    public BaseResponse<>
+    joinUniversityDetail( ){
+    }
+
+}*/
--- src/main/java/com/umc/networkingService/domain/university/controller/UniversityController.java
+++ src/main/java/com/umc/networkingService/domain/university/controller/UniversityController.java
@@ -1,5 +1,78 @@
 package com.umc.networkingService.domain.university.controller;
 
+import com.umc.networkingService.domain.university.service.UniversityService;
+import com.umc.networkingService.global.common.base.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+@Tag(name = "대학교 API", description = "일반 유저용 멤버 관련 API")
+@RestController
+@Validated
+@RequestMapping("/universities")
+@RequiredArgsConstructor
 public class UniversityController {
+
+    private final UniversityService universityService;
+
+    @Operation(summary = "전체 학교 조회 API",description = "전체 학교 조회 API")
+    @GetMapping("")
+    public BaseResponse<>
+    joinUniversityList( ){
+    }
+
+    @Operation(summary = "우리 학교 정보 조회 API",description = "우리 학교 정보 조회 API")
+    @GetMapping("/details")
+    public BaseResponse<>
+    joinUniversityDetail( ){
+    }
+
+    @Operation(summary = "전체 학교 랭킹 조회 API",description = "전체 학교 랭킹 조회 API")
+    @GetMapping("/ranks")
+    public BaseResponse<>
+    joinUniversityRanking( ){
+    }
+
+    @Operation(summary = "학교 전체 기여도 조회 API",description = "학교 전체 기여도 조회 API")
+    @GetMapping("/members")
+    public BaseResponse<>
+    joinUniversityContribution( ){
+    }
+
+    @Operation(summary = "학교 마스코트 조회 API",description = "학교 마스코트 조회 API")
+    @GetMapping("/mascot")
+    public BaseResponse<>
+    joinUniversityMascot( ){
+    }
+
+    @Operation(summary = "학교 마스코트 먹이주기 API",description = "학교 마스코트 먹이주기 API")
+    @PostMapping("/mascot")
+    public BaseResponse<>
+    join( ){
+    }
+
+    @Operation(summary = "학교 생성하기 API",description = "학교 생성하기 API")
+    @PostMapping("")
+    public BaseResponse<>
+    feedUniversityMascot( ){
+    }
+
+    @Operation(summary = "학교 삭제하기 API",description = "학교 삭제하기 API")
+    @DeleteMapping("")
+    public BaseResponse<>
+    deleteUniversity( ){
+    }
+
+    @Operation(summary = "학교 정보 수정하기 API",description = "학교 정보 수정하기 API")
+    @PatchMapping("")
+    public BaseResponse<>
+    patchUniversity( ){
+    }
+
 }
+
