684244ac6dde3e0d545221130924620527c31b20
준써크 <junsuk8960@naver.com>
2024-01-27 19:40:38 +0900

    docs: 깃허브 데이터 조회 API response code 수정

--- src/main/java/com/umc/networkingService/domain/member/controller/MemberController.java
+++ src/main/java/com/umc/networkingService/domain/member/controller/MemberController.java
@@ -78,7 +78,7 @@ public class MemberController {
     @Operation(summary = "깃허브 데이터 조회 API", description = "깃허브 잔디 이미지를 조회하는 API입니다.")
     @ApiResponses( value = {
             @ApiResponse(responseCode = "COMMON200", description = "성공"),
-            @ApiResponse(responseCode = "AUTH008", description = "깃허브 인증이 안 된 사용자일 경우 발생")
+            @ApiResponse(responseCode = "MEMBER005", description = "깃허브 인증이 안 된 사용자일 경우 발생")
     })
     @GetMapping("/github")
     public BaseResponse<MemberInquiryGithubResponse> inquiryGithubImage(@CurrentMember Member member) {
