90396476d8358a81138d40e6a53f4b19e3582713
ShimFFF <shimseawon1510@gmail.com>
2024-02-13 16:21:15 +0900

    feat: 엔티티 연관관계 수정

--- src/main/java/com/umc/networkingService/domain/message/entity/MessageRoom.java
+++ src/main/java/com/umc/networkingService/domain/message/entity/MessageRoom.java
@@ -1,5 +1,6 @@
 package com.umc.networkingService.domain.message.entity;
 
+import com.umc.networkingService.domain.member.entity.Member;
 import com.umc.networkingService.global.common.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -22,12 +23,12 @@ public class MessageRoom extends BaseEntity {
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
-    private Message sender;
+    @JoinColumn(name = "sender_id")
+    private Member sender;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
-    private Message receiver;
+    @JoinColumn(name = "receiver_id")
+    private Member receiver;
 
     @Column(nullable = false)
     private Boolean isAnonymous;
